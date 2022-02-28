8cf905e9c3395a1ab0e9e5e9ac1bd756c73a8a27
Turcanu Adrian <gusky651@gmail.com>
2022-02-28 16:18:59 +0200

    Primul commit

---
+++ src/ProcesareText.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class ProcesareText
+{
+    public static void main(String[] args)
+    {
+        String filePath = "input.txt";
+
+        String content = null;
+        try {
+            content = Files.lines(Paths.get(filePath))
+                    .collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String procesare = content.replaceAll("\\p{P}", ""); //eliminare semne de punctuatie
+
+        System.out.println(procesare);
+    }
+}
